a507b7c29e993fa5450fc6df631c50d8bd863000
= <=>
2024-11-07 11:48:16 -0300

    Lectura

--- LEER/main.py
+++ LEER/main.py
@@ -4,15 +4,14 @@ from ast import literal_eval
 import base_datos as db
 
 MOSTRAR_CAMARA = True
-LEER_IMAGEN = True
+LEER_IMAGEN = False
 IMAGEN = cv2.imread('ejemplo-codigo.jpeg')
 
 # Iniciar la camara
 INDICE_CAMARA = 1
 camara = cv2.VideoCapture(INDICE_CAMARA)
 
-se_leyo = True
-while camara.isOpened() and se_leyo:
+while camara.isOpened():
     ret, frame = camara.read()
     
     if not ret:
@@ -22,25 +21,21 @@ while camara.isOpened() and se_leyo:
         cv2.imshow('Frame', frame)
 
     # ENTRE LEER DESDE LA CAMARA O LEER DESDE IMAGEN
-    if LEER_IMAGEN:
-        h, w  = IMAGEN.shape[:2]
-        qr_code = decode((IMAGEN[:, :, :1].tobytes(), w, h))[0]
-    else:
-        qr_code = decode(frame)[0]
-    
-    data: dict[str, str] = literal_eval(qr_code.data.decode('utf-8'))
+    decoded_frame = decode(frame if not LEER_IMAGEN else (IMAGEN[:, :, :1].tobytes(), *IMAGEN.shape[:2]))
 
-    print(data)
-    
-    if (es_valido := data.get('QR_EXPO')) is not None and es_valido == "true":
-        se_leyo = False
-    
-    if not se_leyo:
-        break
+    if len(decoded_frame) > 0:
+        qr_code = decoded_frame[0]
+
+        data: dict[str, str] = literal_eval(qr_code.data.decode('utf-8'))
+
+        print(data)
+        
+        if (es_valido := data.get('QR_EXPO')) is not None and es_valido == "true":
+            break
     
     # Extraemos las coordenadas de la caja que encierra el QR
-    (x, y, w, h) = qr_code.rect
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        (x, y, w, h) = qr_code.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     if cv2.waitKey(1) & 0xFF == 32:
         break
